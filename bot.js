267adb22e42616bcfacd33b8971ba82f137ef7d0
Narkizzz <39167656+Narkizzz@users.noreply.github.com>
2018-05-11 13:01:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('creator', () => {
-    console.log('Sunt creat de Uktss!');
+client.on('ready', () => {
+    console.log('I am ready!');
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === 'creator') {
+    	message.reply('Sunt creat de Uktss! :D');
   	}
 });
 
